12da04f70f3ad9ca7afe304a765a5c74740ecd46
Veit <ehero00123@163.com>
2022-03-08 22:06:36 +0800

    改好了

--- Bidirectional_circular_linked_lists/source/main.c
+++ Bidirectional_circular_linked_lists/source/main.c
@@ -5,7 +5,7 @@ int main(int argc, char const ** arrv)
 	int i;
 	PNODE mylist = init_list();//生成头节点
 	show(mylist);		//遍历输出
-	inti_prnx(mylist);// 插入新节点
+	inti_prnx(mylist);// 插入新节点gai
 	show(mylist);
 	printf("请输入要查找结点的值:");
 	scanf("%d",&i);
